7b899cb4741b083bedfeae0ab19c2c6624c3ddfd
Ethan Hansen <hanseneg@hotmail.com>
2021-06-09 12:05:33 -0600

    commit6

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,16 @@ import { ReactComponent as OpenMenu } from '../icons/menu.svg'
 
 export default function Navbar(){
 
+    //state to toggle the hamburger and close/X icon
     const [click, setClick] = useState(false)
 
+    //function to handle toggling the icons
     function handleClick(){
         setClick(!click)
     }
 
+    //function that closes the menu once something 
+    //is clicked in the menu by setting click to false
     function closeMenu(){
         setClick(false)
     }
@@ -19,6 +23,8 @@ export default function Navbar(){
             <div>
                 <img className='logo' src='https://pocketrn.com/static/media/Logo.b9fcd954.png' alt='logo' />
             </div>
+            {/* this div checks click in state and uses a ternary to show the 
+            close/X icon or the hamburger icon */}
             <div className='menu' onClick={handleClick}>
                 {click ? (
                     <CloseMenu />
@@ -26,6 +32,9 @@ export default function Navbar(){
                     <OpenMenu />
                 )}
             </div>
+            {/* the ul here uses a ternary to toggle between 2 classes and
+             changes the name of the class depending on the state of click. 
+             If its active then it is displayed using opacity set to 1. */}
             <div className='list'>
                 <ul onClick={closeMenu} className= {click ? 'options active' : 'options'} >
                     <li className='home'><span>Home</span></li>
